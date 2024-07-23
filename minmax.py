c2f1779e8e279f213b90f748bc1f92455f800c02
Lolakshi M <114298855+lolakshi51@users.noreply.github.com>
2024-07-24 04:41:11 +0530

    Update minmax.py

--- minmax.py
+++ minmax.py
@@ -23,3 +23,16 @@ values = [3, 5, 2, 9, 12, 5, 23, 23]
 # Start the Min-Max algorithm
 result = minmax(0, 0, True, values, float('-inf'), float('inf'))
 print("The optimal value is:", result)
+
+#3
+#Visualize the n-dimensional data using heat-map.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv("ToyotaCorolla.csv")
+
+#heat map
+sns.heatmap(data[["Price","KM","Doors", "Weight"]].corr(),cmap='jet',annot=True)
+plt.show()
