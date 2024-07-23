f263d09263018b673a76a06f410c647a4a98632a
Lolakshi M <114298855+lolakshi51@users.noreply.github.com>
2024-07-24 04:43:20 +0530

    Update alphabeta.py

--- alphabeta.py
+++ alphabeta.py
@@ -29,3 +29,19 @@ values = [3, 5, 2, 9, 12, 5, 23, 23]
 # Start the Alpha-Beta Pruning algorithm
 result = alphabeta(0, 0, True, values, float('-inf'), float('inf'))
 print("The optimal value is:", result)
+
+# Visualize the n-dimensional data using Box-plot.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv("ToyotaCorolla.csv")
+
+#box plot
+plt.title('Box Plot')
+plt.boxplot([data["Price"],data["HP"],data["KM"]])
+
+plt.xticks([1,2,3],["Price","HP","KM"])
+
+plt.show()
